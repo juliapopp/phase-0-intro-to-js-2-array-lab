7e1d950a3625846729af5636a7850b6758616c98
Julia Popp <juliampopp@gmail.com>
2022-11-11 14:39:26 -0800

    commit tree d72ea28a0e56befdeb1a6a3b38b74c2d065a3247

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+};
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+};
+function destructivelyRemoveLastCat() {
+    cats.pop()
+};
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+};
+function appendCat(name) {
+    const newCat = [...cats, name];
+    return newCat;
+};
+function prependCat(name) {
+    const catNew = [name, ...cats];
+    return catNew;
+};
+function removeLastCat() {
+    const lostCat = cats.slice(0, -1)
+    return lostCat;
+};
+function removeFirstCat() {
+    const lostFirstCat = cats.slice(1)
+    return lostFirstCat;
+}
